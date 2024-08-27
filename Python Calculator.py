d70e53662dd9e5b2a51b1dcdae9b1a340468963c
Jeffin <162442638+Jefnj@users.noreply.github.com>
2024-08-27 13:34:46 +0530

    Add files via upload

---
+++ Python Calculator.py
@@ -0,0 +1,37 @@
+# Python Calculator
+# operator = input("Enter an operator ( + - * / ):")
+# num1 =int(input("Enter the first number"))
+# num2 =int(input("Enter the second number"))
+# if operator == "+":
+#     result=num1+num2
+#     print(result)
+# elif operator == "-":
+#     result=num1-num2
+#     print(result)
+# elif operator == "*":
+#     result= num1*num2
+#     print(result)
+# elif operator == "/":
+#     result=num1/num2
+#     print(result)
+# else:
+#     print(f"The {operator} you have entered is not valid")
+
+# operator = input("Enter the operator (+ - * /)")
+# num1 = int(input("Enter the number"))
+# num2 = int(input("Enter the number"))
+# if operator == "+":
+#     print(num1+num2)
+# elif operator == "-":
+#     print(num1-num2)
+# elif operator == "*":
+#     print(num1*num2)
+# elif operator == "/":
+#     print(num1/num2)
+# else:
+#     print("You have entered the wrong operator")
+
+
+
+a = "Jeffin"
+print(not a.isalpha())
