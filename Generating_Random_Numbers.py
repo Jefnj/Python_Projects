ee647d90c79869804bcac726ef403df47d8fa398
Jeffin <162442638+Jefnj@users.noreply.github.com>
2024-09-04 16:15:43 +0530

    Add files via upload

---
+++ Generating_Random_Numbers.py
@@ -0,0 +1,16 @@
+#import random
+#print(help(random))
+# low = 1
+# high = 100
+#option = ("rock","paper","scissors")
+# cards = ["2","3","4","5","6","7","8","9","10","J","Q","K","A"]
+# random.shuffle(cards)
+# #number = random.randint(1,20)
+# #number = random.randint(low,high)
+# # number = random.random()   # for random floating number between 0 and 1.
+# # print(number)
+# # a= random.choice(option)
+# # print(a)
+# print(cards)
+
+
